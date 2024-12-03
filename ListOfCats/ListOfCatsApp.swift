a66b03926fec1011958ed0ebca13432344870781
Misha <Mishka.ganin@mail.ru>
2024-12-03 22:32:42 +0300

    init

---
+++ ListOfCats/ListOfCatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfCatsApp.swift
+//  ListOfCats
+//
+//  Created by Михаил Ганин on 03.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ListOfCatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
